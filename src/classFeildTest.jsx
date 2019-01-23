713cc6a727c73db17ba5cbfab0600fbe6a157625
md kamran siddique <ksid0001@ipv6-test.local>
2019-01-23 15:52:17 +0530

    Adding comments about getDerivedStateFromProps lifecycle methods

--- src/classFeildTest.jsx
+++ src/classFeildTest.jsx
@@ -6,6 +6,20 @@ export default class Test extends React.Component{
         super()
         console.log(`This is called with ${this.x}`)
     }
+    /**
+     * 
+     * Warning: `Test` uses `getDerivedStateFromProps` but its initial state is undefined. This is not recommended. Instead, define the initial state by assigning an object to `this.state` in the constructor of `Test`. This ensures that `getDerivedStateFromProps` arguments have a consistent shape
+     * 
+     */
+    /**
+     * 
+     * Warning: Test.getDerivedStateFromProps(): A valid state object (or null) must be returned. You have returned undefined.
+     * 
+     * 
+     */
+    static getDerivedStateFromProps(){
+        console.log('Hi i am in getDerivedStateFromProps')
+    }
     render(){
         return(
             <div>Hi I am here</div>
